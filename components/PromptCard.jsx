bcef83b4e8fdc931b41e60c7a0f1c7bcdbe82b92
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-11-17 15:12:03 +0530

    Revert "bug fix: minor"
    This reverts commit d0e91e64cc471b9442a8b3e359bf6cd08a7aaab1.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -25,11 +25,8 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   };
 
   return (
-    <div className="prompt_card">
-      <div
-        className="flex justify-between items-start gap-5"
-        onClick={handleChangeProfile}
-      >
+    <div className="prompt_card" onClick={handleChangeProfile}>
+      <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start itesm-center gap-3 cursor-pointer">
           <Image
             src={post.creator.image}
