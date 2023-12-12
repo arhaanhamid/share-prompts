02b4653b26501ef4bebdf890d6f8b66eab496504
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-12-12 12:25:57 +0530

    changed name from promptopia to share prompts

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promtopia",
+  title: "Share Prompts",
   description: "Discover & Share AI Prompts",
 };
 
